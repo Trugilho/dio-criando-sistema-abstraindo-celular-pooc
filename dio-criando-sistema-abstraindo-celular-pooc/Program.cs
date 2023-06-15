ee67cd114ef73121d8acbb4758d35408d52fed7b
Roberto Trugilho Moreira <105746425+Trugilho@users.noreply.github.com>
2023-06-15 08:10:57 -0300

    Add files via upload

---
+++ dio-criando-sistema-abstraindo-celular-pooc/Program.cs
@@ -0,0 +1,21 @@
+﻿using DesafioPOO.Models;
+
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(27) 9934-3456", "Tijolão", "00000000", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+
+Iphone iphone = new Iphone("(27) 9876-1234", "iPhone 15", "7777777", 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("facebook");
+
+
